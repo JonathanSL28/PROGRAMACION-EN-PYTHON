2b0765c6b919b811041faf36f1f80fb2d662e26d
JonathanSL28 <85839428+JonathanSL28@users.noreply.github.com>
2021-07-18 08:41:57 -0500

    Add files via upload

---
+++ Ejercicios python/EJERCICIO-CARGO.py
@@ -0,0 +1,14 @@
+
+class Cargo:
+    def __init__(self):
+        self.codigo = 0
+        self.descripcion = ''
+
+carg1 = Cargo()
+carg1.codigo = 1
+carg1.descripcion = 'Docente'
+print(carg1.codigo, carg1.descripcion)
+cargos2 = Cargo()
+cargos2.codigo = 2
+cargos2.descripcion = 'Analista'
+print(cargos2.codigo, cargos2.descripcion)
